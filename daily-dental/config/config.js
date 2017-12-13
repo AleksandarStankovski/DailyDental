f2a41226efdbaa30fc860c369087773a14aac75e
Aleksandar <aleksandar.stankowski@gmail.com>
2017-12-13 15:45:54 +0200

    Documentation

--- daily-dental/config/config.js
+++ daily-dental/config/config.js
@@ -4,7 +4,6 @@ module.exports = {
     development: {
         port: 3000,
         rootFolder: path.normalize(path.join(__dirname, '/../')),
-        connectionString: 'mongodb://localhost:27017/dailyDental',
-        appointmentStatus: ['confirmed', 'unconfirmed', 'arrived', 'current', 'finished', 'late', 'missing', 'canceled', 'urgent']
+        connectionString: 'mongodb://localhost:27017/dailyDental'
     }
 };
